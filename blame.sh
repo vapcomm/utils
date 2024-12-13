7b87f378cb386fc3edc8723387ad0744ce972446
vapcomm <68540035+vapcomm@users.noreply.github.com>
2024-12-14 00:57:15 +0400

    Added Elixir source files extentions

--- blame.sh
+++ blame.sh
@@ -4,7 +4,7 @@
 #
 # Usage:
 # 1. Edit line with `find . -name ...` with your project source files extentions.
-#    This example count Kotlin, Swift, Markdown and Docker files for KMM projects.
+#    This example count Kotlin, Swift, Elixir, Markdown and Docker files.
 # 2. Start blame.sh in the project's root with .git directory.
 # 3. Don't blame yourself for low lines counts, gold is between lines ;)
 #
@@ -15,7 +15,7 @@ TMP_FILE="__blame.txt"
 echo Create big blame file...
 
 rm -f $TMP_FILE
-find . -name "*Dockerfile" -o -name "*.kts" -o -name "*.md" -o -name "*.kt" -o -name "*.swift" | xargs -n1 git blame -e >>$TMP_FILE
+find . -name "*Dockerfile" -o -name "*.kts" -o -name "*.md" -o -name "*.kt" -o -name "*.swift" -o -name "*.ex" -o -name "*.exs" | xargs -n1 git blame -e >>$TMP_FILE
 
 DEVELOPERS=`cat $TMP_FILE | sed -n 's/^.*(<\([^>]*\)>.*$/\1/p' | sort | uniq`
 
